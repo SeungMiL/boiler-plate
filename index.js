f0529c1c39dd63f4cd91232cbde1ff629cd2a786
SeungMiL <89256060+SeungMiL@users.noreply.github.com>
2022-08-27 17:37:49 +0900

    2

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 5000
+// const bodyParser = require('body-parser');
+const config = require('./config/key')
+
+const { User } = require('./models/User');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin:123789456@boilerplate.4keaiip.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
@@ -12,9 +19,23 @@ mongoose.connect('mongodb+srv://admin:123789456@boilerplate.4keaiip.mongodb.net/
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! seungmin')
 })
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        });
+    });
+
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
